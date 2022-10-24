1c675ce1fc46938e2dfc96c67e60de371f06d205
janfabius <adanet18@gmail.com>
2022-10-24 11:26:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/logingui/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.logingui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        getSupportActionBar().setTitle("Log in");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+}

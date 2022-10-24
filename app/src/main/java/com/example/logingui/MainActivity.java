c32ec06eda26133ae6c4570a21aae43a0d18c0da
janfabius <adanet18@gmail.com>
2022-10-24 13:44:57 +0200

    upg

--- app/src/main/java/com/example/logingui/MainActivity.java
+++ app/src/main/java/com/example/logingui/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.logingui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    ImageButton sync;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,28 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Log in");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        login = findViewById(R.id.log_login);
+        sync = findViewById(R.id.imageBtn_sync);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // loggare sul database accesso
+
+                // cambiare in base alla propria app
+                Intent scIntent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(scIntent);
+            }
+        });
+
+
+
+
+
+
 
 
 
